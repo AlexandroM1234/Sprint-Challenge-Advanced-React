a53f5b7fff94a2d73992eec1d16b581dbfc86bd3
Alex Martinez <alexmartinez0071234@gmail.com>
2020-03-13 11:24:52 -0400

    had the players name country searches render to dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players").then(response => {
+      this.setState({
+        players: response.data
+      });
+      // console.log(this.state.players);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.players.map(player => {
+          return (
+            <div>
+              <p>{player.name}</p>
+              <p>{player.country}</p>
+              <p>{player.searches}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
